d69c7de38d8001ed4a58bc073876bec7ff4d2130
FatemehAllameh <fatemeh.allameh19@gmail.com>
2026-07-20 10:45:57 +0330

    feat: implement responsive header

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
-import './style.css'
+import "./style.css";
+
+// Header
+const hamburgerBtn = document.querySelector(".hamburger-btn");
+const navMenuMobile = document.querySelector(".nav-menu-mobile");
+let isOpen = false;
+hamburgerBtn.addEventListener("click", () => {
+  if (isOpen) {
+    hamburgerBtn.classList.remove("open");
+    navMenuMobile.classList.add("-left-50", "md:-left-70");
+    navMenuMobile.classList.remove("left-0");
+    isOpen = false;
+  } else {
+    hamburgerBtn.classList.add("open");
+    navMenuMobile.classList.remove("-left-50", "md:-left-70");
+    navMenuMobile.classList.add("left-0");
+    isOpen = true;
+  }
+});
