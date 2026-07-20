e78b319ad159b2365a9d729322570fee7af0fbc4
FatemehAllameh <fatemeh.allameh19@gmail.com>
2026-07-20 20:34:07 +0330

    feat: add intro section layout

--- src/main.js
+++ src/main.js
@@ -3,17 +3,20 @@ import "./style.css";
 // Header
 const hamburgerBtn = document.querySelector(".hamburger-btn");
 const navMenuMobile = document.querySelector(".nav-menu-mobile");
+const overlay = document.querySelector(".overlay");
 let isOpen = false;
 hamburgerBtn.addEventListener("click", () => {
   if (isOpen) {
     hamburgerBtn.classList.remove("open");
     navMenuMobile.classList.add("-left-50", "md:-left-70");
     navMenuMobile.classList.remove("left-0");
+    overlay.classList.remove("active");
     isOpen = false;
   } else {
     hamburgerBtn.classList.add("open");
     navMenuMobile.classList.remove("-left-50", "md:-left-70");
     navMenuMobile.classList.add("left-0");
+    overlay.classList.add("active");
     isOpen = true;
   }
 });
